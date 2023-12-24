d36ddd73f88dea2f4e6a157f107cade73ba2389e
Isaac <zyskson@gmail.com>
2023-12-24 11:29:36 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ThemeProvider } from '#/components/theme-provider'
+import { Analytics } from '#/components/analytics'
+import { TailwindIndicator } from '#/components/tailwind-indicator'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +18,15 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <head/>
+      <body className={inter.className}>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem >
+        {children}
+        <Analytics/>
+        <TailwindIndicator/>
+        </ThemeProvider>
+        </body>
     </html>
   )
 }
