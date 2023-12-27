5ce6eccf72edf1ecfd1e8f32c21b78a58289cdc3
Isaac <zyskson@gmail.com>
2023-12-27 11:30:56 +0100

    first commit

---
+++ components/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+function header({}: Props) {
+  return (
+    <header className='sticky top-0 bg-green-300'>
+
+    </header>
+  )
+}
+
+export default header
